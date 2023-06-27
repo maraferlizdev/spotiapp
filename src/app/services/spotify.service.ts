59b8e5f1f1673461248270c1a134611c9bed601a
maraferlizdev <maraferlizdev@gmail.com>
2023-06-27 16:09:15 -0600

    update images, token

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
   
-   token= 'BQBKKmMmwd4BQCcTuXCz6l4bXxIjrPfy8g-kylicepyHQfLJImxUK3nBt_um2ruJ6WtCOY8uRFhoZ6eh98aI9DLlTOLnwahFTLM9QUg9yvdJ3UrBRUxG6Xbq6rcAuMvWtkkgyOfWgW8OykgGOXXRaWa3yN2hN8eIpC5HTEV3QJJ5aoS_hytzxBz_NyE9EwCCY5nF-JpgmElaisalbnzDVv0dx8_ZYCYZ8-dgMUBQCbltRHoz-fDqkBXYMnFnV7VMnIIPuInYDF-w8GZpljZzHYMHIK9aam7rwW7gi4Hw1Lmmju6Sd7GiVFenLzGMSogt8RMa_Tfk2OrkgshrJ3h7ratww';
+   token= 'BQBCFY0QwHJbtgtOnoxRlxxPlFOx5d4JECul_yEXGLm1RTtkbHXMj4pzZ5vIPBmryyQPsYE7Vgc5bDkN7D9Baz5CYCEmoWGuZOkhasis0q_Jvr7xLwU';
 
   constructor(private http: HttpClient) {
     console.log('Spotify service listo');
